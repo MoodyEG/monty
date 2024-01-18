5d552e780b196dfd864764b8ba7ea4048ca7b484
Mostaaaaa <mostafa.ali.ahmed@gmail.com>
2024-01-18 12:34:28 -0800

    update push_stack.c

--- push_stack.c
+++ push_stack.c
@@ -54,6 +54,30 @@ void top_print(stack_t **head, unsigned int ln)
 }
 
 /**
+ * swap_stack -  swaps the top two elements of the stack.
+ * @head: head of the list
+ * @ln: line number
+ * Return: no return
+ */
+void swap_stack(stack_t **head, unsigned int ln)
+{
+	int i;
+
+	if (*head == NULL || head == NULL || (*head)->next == NULL)
+		error_f2(*head, 7, ln);
+	i = (*head)->n;
+	(*head)->n = ((*head)->next)->n;
+	((*head)->next)->n = i;
+	/* sec = (*head)->next; */
+	/* (*head)->next = sec->next; */
+	/* (*head)->prev = sec; */
+	/* sec->prev = NULL; */
+	/* sec->next = *head; */
+	/* *head = sec; */
+
+}
+
+/**
  * free_stack - frees a stack_t list.
  * @head: head of the list
  * Return: no return
